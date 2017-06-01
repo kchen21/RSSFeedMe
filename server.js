fe159b8e45594faf2ebbed302b5304d8c41aa639
Kenneth Chen <kennethchendbgt@gmail.com>
2017-06-01 13:51:08 -0400

    Install HTTP request logger middleware morgan

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 let express = require('express');
+let morgan = require('morgan');
 
 let app = express();
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.json("Welcome to RSSFeedMe!");
+});
 
 app.listen(3000, (err) => {
   if (err) throw err;
