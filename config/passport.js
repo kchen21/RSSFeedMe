2e808caffbf8b8ea96768d5d8d0ed09e21cc8323
Kenneth Chen <kennethchendbgt@gmail.com>
2017-06-05 16:59:43 -0400

    Write logic for local login

--- config/passport.js
+++ config/passport.js
@@ -1,2 +1,40 @@
 let passport = require('passport');
 let LocalStrategy = require('passport-local').Strategy();
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+passport.use('local-login', new LocalStrategy({
+  usernameField: 'username',
+  passwordField: 'password',
+  passReqToCallback: true
+}, (req, username, password, done) => {
+  User.findOne({ username: username }, (err, user) => {
+    if (err) return done(err);
+
+    if (!user) {
+      return done(null, false, req.flash('loginMessage', 'No user has been found'));
+    }
+
+    if (!user.comparePassword(password)) {
+      return done(null, false, req.flash('loginMessage', 'Password is incorrect'));
+    }
+
+    return done(null, user);
+  });
+}));
+
+exports.isAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect('/welcome');
+};
