dd0ecac84a3df62fc0dced5d4258a1f9bd1f5790
Kenneth Chen <kennethchendbgt@gmail.com>
2017-06-09 15:14:00 -0400

    Show collections in sidebar

--- models/personal_collection.js
+++ models/personal_collection.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let PersonalCollectionSchema = new Schema({
-  title: { type: String, required: true, unique: true },
+  title: { type: String, required: true },
   user: { type: Schema.Types.ObjectId, ref: 'User' },
   feeds: [{ type: Schema.Types.ObjectId, ref: 'Feed' }]
 });
