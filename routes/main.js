673f223652b2e5b609b192ce0447bfb793d0e061
Kenneth Chen <kennethchendbgt@gmail.com>
2017-06-22 18:20:42 -0400

    Write function that uses AJAX request to get articles from feed

--- routes/main.js
+++ routes/main.js
@@ -71,4 +71,22 @@ router.post('/subscribe', (req, res, next) => {
   });
 });
 
+router.get('/feed/:feed_id', (req, res, next) => {
+  Feed.findOne({ _id: req.params.feed_id }, (err, feed) => {
+    if (err) return next(err);
+
+    const getArticles = (url) => {
+      return ($.ajax({
+        type: "GET",
+        url: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D%22" + encodeURIComponent(`${url}`) + "%22&format=json&diagnostics=true&callback=",
+        dataType: 'json'
+      }));
+    };
+
+    return getArticles(feed.xml_url).then((feedData) => {
+      res.json(feedData);
+    });
+  });
+});
+
 module.exports = router;
