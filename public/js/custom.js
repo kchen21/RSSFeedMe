6e5ea04bb9262223e09f7f52f81d633ff1e4b6f2
Kenneth Chen <kennethchendbgt@gmail.com>
2017-07-07 16:31:18 -0400

    Create modal for feed removal

--- public/js/custom.js
+++ public/js/custom.js
@@ -6,4 +6,16 @@ $(function() {
     $("#subscribeModal #feedId").val(myFeedId);
   });
 
+  $(document).on("click", ".delete-btn", function(e) {
+    e.preventDefault();
+    var myCollectionId = $(this).data('collectionid');
+    var myCollectionTitle = $(this).data('collectiontitle');
+    var myFeedId = $(this).data('feedid');
+    var myFeedTitle = $(this).data('feedtitle');
+    $("#deleteFeedModal #collectionId").val(myCollectionId);
+    $("#deleteFeedModal #collectionTitle").val(myCollectionTitle);
+    $("#deleteFeedModal #feedId").val(myFeedId);
+    $("#deleteFeedModal #feedTitle").val(myFeedTitle);
+  });
+
 });
