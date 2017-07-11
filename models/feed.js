3c00dc8d4bda8085a1a02735232aa203e1b91a99
Kenneth Chen <kennethchendbgt@gmail.com>
2017-07-11 14:15:21 -0400

    Add articles to Feed model

--- models/feed.js
+++ models/feed.js
@@ -6,7 +6,8 @@ let FeedSchema = new Schema({
   title: { type: String, required: true },
   description: String,
   link: String,
-  image_url: String
+  image_url: String,
+  articles: [{ type: Schema.Types.ObjectId, ref: 'Article' }]
 });
 
 module.exports = mongoose.model('Feed', FeedSchema);
